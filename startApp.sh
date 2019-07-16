b4dd755fcfdccb1d7fa877b41273112f96a177ea
Stan Zhou <stan.osaka@gmail.com>
2019-07-16 20:05:03 +1000

    Update startApp.sh file

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.us-west-2.rds.amazonaws.com
-export DB_PRD_USER=spidertutorial
-export DB_PRD_PASS=spidertutorial
+export DB_PRD_HOST=stantest-postgresql.c3mzoji03zxf.ap-southeast-2.rds.amazonaws.com
+export DB_PRD_USER=stantest
+export DB_PRD_PASS=mario54321
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
